62ce00fa545bd52c9b97380d857bd97d9e37f4b1
itsPareshY <itspareshyadav@gmail.com>
2024-11-14 08:45:00 +0530

    JumpGame

--- src/main/java/com/paresh/practice/array/problems/JumpGame.java
+++ src/main/java/com/paresh/practice/array/problems/JumpGame.java
@@ -3,24 +3,22 @@ package com.paresh.practice.array.problems;
 public class JumpGame {
 
     public boolean canJump(int[] nums) {
-        int sumOfJumps = 0;
-        for(int i = 0 ; i< nums.length - 1; i ++){
-            if(i > nums.length -3){
-                break;
+        int maxReach = 0;
+        for(int i = 0 ; i< nums.length ; i ++){
+            if(i > maxReach){
+                return false;
             }
-            if(nums[i] == nums.length - 1){
-                return true;
+            maxReach = Math.max(maxReach,i+nums[i]);
+            if(maxReach >= nums.length -1 )
+            {
+                return  true;
             }
-            else if(nums[i] < nums.length - 1){
-                p
-            }
-
         }
         return false;
     }
 
     public static void main (String args[]){
-        int[] nums = {2,3,1,1,3};
+        int[] nums = {3,2,1,0,3};
 
         System.out.println(new JumpGame().canJump(nums));
     }
