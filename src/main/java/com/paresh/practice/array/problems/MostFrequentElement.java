45d73ca45e0ff700ee683cc908931d652f566653
itsPareshY <itspareshyadav@gmail.com>
2025-02-12 22:15:39 +0530

    Some practice - still need to complere SwitchNodes

---
+++ src/main/java/com/paresh/practice/array/problems/MostFrequentElement.java
@@ -0,0 +1,40 @@
+package com.paresh.practice.array.problems;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MostFrequentElement {
+    public static void main(String args[]) {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(8);
+        list.add(5);
+        list.add(12);
+        list.add(20);
+        list.add(20);
+        list.add(20);
+        list.add(25);
+        list.add(25);
+        list.add(50);
+        list.add(50);
+        list.add(50);
+        list.add(50);
+
+        list.stream().collect(Collectors.groupingBy(item -> item, Collectors.counting()))
+                .entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue())) //TODO: Important how we are comparing e2.getValue() with e1.getValue()
+                .limit(5)
+                .forEach(entry -> System.out.print(entry.getKey() + " "));
+
+        //output: [50, 20, 25, 2, 5]
+        System.out.println();
+        list.stream().collect(Collectors.groupingBy(item -> item, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getValue)) //TODO: Another way to sort
+                .limit(5)
+                .forEach(entry -> System.out.print(entry.getKey() + " "));
+    }
+}
---
+++ src/main/java/com/paresh/practice/data/structure/SwitchNodes.java
@@ -0,0 +1,19 @@
+package com.paresh.practice.data.structure;
+
+public class SwitchNodes {
+
+    public static void main (String args[]){
+    LinkedListSelfPractice<Integer> list = new LinkedListSelfPractice<>();
+    list.append(1);
+    list.append(2);
+    list.append(3);
+    list.append(4);
+    list.append(5);
+    list.append(6);
+    list.print();
+
+    //move last element to second position, 1 -> 6 -> 2 -> 5 -> 3 -> 4 -> null
+
+
+    }
+}
