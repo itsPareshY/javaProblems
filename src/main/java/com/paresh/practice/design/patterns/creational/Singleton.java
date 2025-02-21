3aba65dd2c2879df15b1318fbd3981441f1c7f83
itsPareshY <itspareshyadav@gmail.com>
2025-02-21 06:49:31 +0530

    Add Externalizable class with TODOs for serialization methods and refactor Singleton package structure

--- src/main/java/com/paresh/practice/design/patterns/creational/Singleton.java
+++ src/main/java/com/paresh/practice/design/patterns/creational/Singleton.java
@@ -1,4 +1,4 @@
-package com.paresh.practice.design.patterns;
+package com.paresh.practice.design.patterns.creational;
 
 import java.io.Serializable;
 
---
+++ src/main/java/com/paresh/practice/java/examples/Externalizable.java
@@ -0,0 +1,10 @@
+package com.paresh.practice.java.examples;
+
+public class Externalizable {
+    //TODO externalize only few fields
+    //TODO implement readExternal and writeExternal methods
+    //TODO implement readObject and writeObject methods
+    //TODO implement readObjectNoData method
+    //TODO implement readResolve method
+    //TODO implement writeReplace method
+}
