74b9d01888aa1aa077cba0cd423329784fd0c3f8
itsPareshY <itspareshyadav@gmail.com>
2025-03-01 15:22:01 +0530

    Refactor multi-stage pipeline execution to use a single method for task coordination

--- src/main/java/com/paresh/practice/mutithreading/advanced/countdown/MultiStagePipelineEx.java
+++ src/main/java/com/paresh/practice/mutithreading/advanced/countdown/MultiStagePipelineEx.java
@@ -17,44 +17,31 @@ public class MultiStagePipelineEx {
      * @throws InterruptedException if any thread is interrupted while waiting
      */
     public static void main(String... args) throws InterruptedException {
-        CountDownLatch latchStageTwo = new CountDownLatch(1);
-        CountDownLatch latchStageThree = new CountDownLatch(1);
 
-
-        Task3 task3 = new Task3(latchStageTwo);
-        Task4 task4 = new Task4(latchStageThree);
+        Task1 task1 = new Task1();
+        Task2 task2 = new Task2();
+        Task3 task3 = new Task3();
+        Task4 task4 = new Task4();
 
         ExecutorService executorService = Executors.newCachedThreadPool();
 
-        executeStageOne(executorService);
-        executeStageTwo(executorService, task3, latchStageTwo);
-        executeStateThree(executorService, task4, latchStageThree);
+        executeStage(executorService, "1", task1 , task2);
+        executeStage(executorService, "2", task3);
+        executeStage(executorService, "3", task4);
         executorService.shutdown();
     }
 
-    private static void executeStateThree(ExecutorService executorService, Task4 task4, CountDownLatch latchStageThree) throws InterruptedException {
-        System.out.println("Stage 3 Started ...");
-        executorService.execute(task4);
-        latchStageThree.await();
-        System.out.println("Stage 3 Completed ...");
-    }
-
-    private static void executeStageTwo(ExecutorService executorService, Task3 task3, CountDownLatch latchStageTwo) throws InterruptedException {
-        System.out.println("Stage 2 Started ...");
-        executorService.execute(task3);
-        latchStageTwo.await();
-        System.out.println("Stage 2 Completed ...");
-    }
-
-    private static void executeStageOne(ExecutorService executorService) throws InterruptedException {
-        CountDownLatch latchStageOne = new CountDownLatch(2);
-        Task1 task1 = new Task1(latchStageOne);
-        Task2 task2 = new Task2(latchStageOne);
-        System.out.println("Stage 1 Started ...");
-        executorService.execute(task1);
-        executorService.execute(task2);
-        latchStageOne.await();
-        System.out.println("Stage 1 Completed ...");
+    private static void executeStage(ExecutorService executorService, String stage, Runnable ... tasks) throws InterruptedException {
+        CountDownLatch stageLatch = new CountDownLatch(tasks.length);
+        System.out.printf("Starting Stage %s ...%n", stage);
+        for(Runnable task : tasks) {
+            executorService.execute(() -> {
+                task.run();
+                stageLatch.countDown();
+            });
+        }
+        stageLatch.await();
+        System.out.printf("Stage %s complete.%n", stage);
     }
 }
 
@@ -62,20 +49,6 @@ public class MultiStagePipelineEx {
  * Task1 class representing a task in the first stage.
  */
 class Task1 implements Runnable {
-    CountDownLatch latch;
-
-    /**
-     * Constructor for Task1.
-     *
-     * @param latch CountDownLatch to signal task completion
-     */
-    public Task1(CountDownLatch latch) {
-        this.latch = latch;
-    }
-
-    /**
-     * Run method to execute the task.
-     */
     public void run() {
         System.out.println("Task 1 started ...");
         try {
@@ -84,7 +57,6 @@ class Task1 implements Runnable {
             throw new RuntimeException(e);
         }
         System.out.println("Task 1 completed.");
-        latch.countDown();
     }
 }
 
@@ -92,17 +64,6 @@ class Task1 implements Runnable {
  * Task2 class representing a task in the first stage.
  */
 class Task2 implements Runnable {
-    CountDownLatch latch;
-
-    /**
-     * Constructor for Task2.
-     *
-     * @param latch CountDownLatch to signal task completion
-     */
-    public Task2(CountDownLatch latch) {
-        this.latch = latch;
-    }
-
     /**
      * Run method to execute the task.
      */
@@ -114,7 +75,6 @@ class Task2 implements Runnable {
             throw new RuntimeException(e);
         }
         System.out.println("Task 2 completed.");
-        latch.countDown();
     }
 }
 
@@ -122,17 +82,6 @@ class Task2 implements Runnable {
  * Task3 class representing a task in the second stage.
  */
 class Task3 implements Runnable {
-    CountDownLatch latch;
-
-    /**
-     * Constructor for Task3.
-     *
-     * @param latch CountDownLatch to signal task completion
-     */
-    public Task3(CountDownLatch latch) {
-        this.latch = latch;
-    }
-
     /**
      * Run method to execute the task.
      */
@@ -144,7 +93,6 @@ class Task3 implements Runnable {
             throw new RuntimeException(e);
         }
         System.out.println("Task 3 completed.");
-        latch.countDown();
     }
 }
 
@@ -152,20 +100,6 @@ class Task3 implements Runnable {
  * Task4 class representing a task in the third stage.
  */
 class Task4 implements Runnable {
-    CountDownLatch latch;
-
-    /**
-     * Constructor for Task4.
-     *
-     * @param latch CountDownLatch to signal task completion
-     */
-    public Task4(CountDownLatch latch) {
-        this.latch = latch;
-    }
-
-    /**
-     * Run method to execute the task.
-     */
     public void run() {
         System.out.println("Task 4 started ...");
         try {
@@ -174,6 +108,5 @@ class Task4 implements Runnable {
             throw new RuntimeException(e);
         }
         System.out.println("Task 4 completed.");
-        latch.countDown();
     }
 }
